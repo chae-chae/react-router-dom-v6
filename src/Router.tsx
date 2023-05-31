be88f841bc8667b85687a2068faa927b94ffc2b8
chae-chae <abed_nadir@naver.com>
2023-05-31 17:11:07 +0900

    study createBrowserRouter

---
+++ src/Router.tsx
@@ -0,0 +1,24 @@
+import { Route, Routes, createBrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./screens/Home";
+import About from "./screens/About";
+import Root from "./Root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
+export default router;
